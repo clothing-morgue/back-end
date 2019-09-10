4a6e6cfc28a0210e137e430ac737165eb0dd4f6c
Joe <yogurmystery@gmail.com>
2019-09-10 11:08:39 -0500

    Set up get routes for users

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,29 @@
 import { Router } from "express";
+import db from "../../data/dbConfig";
 
 const router = Router();
 
 router.get("/", (req, res) => {
-  return res.send(Object.values(req.context.models.users));
+  db("users")
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      res.status(500).json({ err: "failed to get users", err });
+    });
 });
 
-router.get("/:userId", (req, res) => {
-  return res.send(req.context.models.users[req.params.userId]);
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  db("users")
+    .where({ id })
+    .then((users) => {
+      if (users.length) {
+        res.json(users[0]);
+      } else {
+        res.status(404).json({ err: "invalid user id" });
+      }
+    });
 });
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ import "dotenv/config";
 import cors from "cors";
 import express from "express";
 import cloudinary from "cloudinary";
-import models from "./models";
 import routes from "./routes";
 
 const app = express();
@@ -11,13 +10,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  req.context = {
-    models,
-    me: models.users[1]
-  };
-  next();
-});
+// app.use((req, res, next) => {
+//   req.context = {
+//     models,
+//     me: models.users[1]
+//   };
+//   next();
+// });
 
 app.use("/session", routes.session);
 app.use("/users", routes.users);
