8bb2a44145226bc96dc2b80559f34a577cadf1ea
Joe <yogurmystery@gmail.com>
2019-10-05 09:13:57 -0500

    attempting to rework tag route

--- src/routes/tags.js
+++ src/routes/tags.js
@@ -26,34 +26,49 @@ router.post("/", async (req, res, next) => {
       });
     }
   }
+  let thisTag = await Tags.getTagByName(tag.tagName);
 
-  if (await Tags.canAddTag(tag.tagName)) {
-    try {
-      const tagObj = await Tags.createTag({ tagName: tag.tagName });
-      const taggedProd = await Tags.attachTag({
-        tag_id: tagObj.id,
-        product_id: tag.product_id
-      });
-      res.status(201).json({ taggedProd });
-    } catch (error) {
-      return next(error);
-    }
-  } else {
-    try {
-      const tagObj = await Tags.getTagByName(tag.tagname);
-      const taggedObj = {
-        tag_id: tagObj.id,
-        product_id: tag.product_id
-      };
-      Tags.attachTag(taggedObj).then((tagged) => {
-        res.json({ tagged: tagged });
-      });
-    } catch (error) {
-      res
-        .status(500)
-        .json({ message: "Couldn't assign that tag", error: error });
-    }
-  }
+  thisTag ? 
+    Tags
+      .attachTag({ 
+        tag_id: thisTag.id, 
+        product_id: tag.product_id })
+      .then((tagged) => {
+          res.status(201).json(tagged);
+        })
+      .catch((error) => {
+        res
+          .status(error)
+          .json({ message: "trouble attaching tag", error: error });
+        })
+    : res.status(500).json({ message: 'That tag does not exist'});
+  // if (await Tags.canAddTag(tag.tagName)) {
+  //   try {
+  //     const tagObj = await Tags.createTag({ tagName: tag.tagName });
+  //     const taggedProd = await Tags.attachTag({
+  //       tag_id: tagObj.id,
+  //       product_id: tag.product_id
+  //     });
+  //     res.status(201).json({ taggedProd });
+  //   } catch (error) {
+  //     return next(error);
+  //   }
+  // } else {
+  //   try {
+  //     const tagObj = await Tags.getTagByName(tag.tagname);
+  //     const taggedObj = {
+  //       tag_id: tagObj.id,
+  //       product_id: tag.product_id
+  //     };
+  //     Tags.attachTag(taggedObj).then((tagged) => {
+  //       res.json({ tagged: tagged });
+  //     });
+  //   } catch (error) {
+  //     res
+  //       .status(500)
+  //       .json({ message: "Couldn't assign that tag", error: error });
+  //   }
+  // }
   //   Tags.createTag({ tagName: tag.tagName})
   //     .then(tag => {
   //       res.status(201).json({ tag });
